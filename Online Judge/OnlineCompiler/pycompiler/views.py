444a551a6b6617f0ffa46353c1aff32ff363965c
Pulkit Kumar Mathur <97748031+Pulkit1822@users.noreply.github.com>
2023-11-06 12:07:17 +0530

    Update views.py

--- Online Judge/OnlineCompiler/pycompiler/views.py
+++ Online Judge/OnlineCompiler/pycompiler/views.py
@@ -58,7 +58,6 @@ def runcode(request):
             sys.stdout = original_stdout
             output = e
             success_message = None
-
     # Finally, return and render the index page and then send code data and output to show on the page
     return render(request, 'index.html', {"code": codeareadata, "output": output, "execution_time": execution_time,
                                           "memory_usage": memory_usage, "memory_usage_mb": memory_usage_mb,
